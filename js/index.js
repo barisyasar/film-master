0ef20ddd2552c3f8cfc1445253aee0e374539ee3
Barış Yaşar <barisyasar@Bars-MacBook-Pro.local>
2021-02-04 02:44:04 +0300

    3 şubat son

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 var databaseIndex = firebase.database();
 var refIndex = databaseIndex.ref("Haberler");
 var refIndexGunun = databaseIndex.ref("Gunun Filmi");
+var refIndexFilmler = databaseIndex.ref("Filmler");
 
 // Slider
 const carousel_indicators = document.getElementById("carousel-indicators");
@@ -10,22 +11,73 @@ const carousel_inner = document.getElementById("carousel-inner");
 //Gunun Filmi
 const gununFilmiIndex = document.getElementById('gununFilmiIndex');
 
+//Filmler
+const filmler = document.getElementById('filmler');
+
 
 // Definition Function
+const setFilmler = (nameF,imgF,turF,kodF) => {
+    filmler.innerHTML += 
+    `
+    <div class ="col-md-4 p-1 ">
+    <div class="film-item w-100 h-100" data-category="${turF}" name="${kodF}" style="background-color: #000000; color: black;border: 4px solid #ffffff;">
+    <img src="${imgF}" class="w-100 h-100" style="max-height: 270px;">
+    <div class="text-center" style="overflow: hidden;
+    white-space: nowrap;">
+    <h5 class="pt-1" style="color: #DCA424;"><b>${nameF}</b></h5>
+    <p class="bg-light d-block p-1 mx-4 mb-2" style="border-radius: 5px; "><b>${nameF}</b></p>
+    
+
+    </div>
+    </div>
+    </div>
+  
+    `
+;
+}
+
+const getFilmler = (number) => {
+     
+    for (i = 1; i < number + 1; i++) {
+
+        refIndexFilmler.child(i).on("value", function (snapshot) {
+
+            snapshot.forEach(function (data) {
+
+                nameFilm = data.val().filmadi;
+                imgFilm = data.val().resim;
+                turFilm = data.val().filmturu;
+                kodFilm = data.key;
+                setFilmler(nameFilm, imgFilm, turFilm, kodFilm);
+            });
+        });
+    }
+}
+
+const getNumberFilm = (callback) => {
+    var childCountFilm;
+    refIndexFilmler.once("value")
+        .then(function (snapshot) {
+            childCountFilm = snapshot.numChildren();
+            callback(childCountFilm);
+        });
+}
+
 const setGununFilmi = (nameGF, imgGF) => {
     gununFilmiIndex.innerHTML =
         `
-    <div class="p-1" style="background-color: #000000; color: black;border-radius: 5px;" class="w-100 h-100">
-    <img src="${imgGF}" alt="${nameGF}" class="w-100 h-100">
+    <div style="background-color: #000000; color: black;border: 4px solid #ffffff;">
+    <img src="${imgGF}" alt="${nameGF}" class="w-100 h-100" style="max-height: 300px;">
     <div class="text-center" style="overflow: hidden;
     white-space: nowrap;">
-        <h5 class="pt-1" style="color: #DCA424;"><b>Günün Filmi</b></h5>
-        <p class="bg-light d-block m-2 p-1" style="border-radius: 5px; "><b>${nameGF}</b></p>
+    <h5 class="pt-1" style="color: #DCA424;"><b>Günün Filmi</b></h5>
+        <p class="bg-light d-block p-1 mx-4 mb-2" style="border-radius: 5px; "><b>${nameGF}</b></p>
 
     </div>
 </div>
     `
 }
+
 const getGununFilmi = (callback) => {
     var nameGF;
     var imgGF;
@@ -35,10 +87,12 @@ const getGununFilmi = (callback) => {
         callback(nameGF, imgGF);
     });
 }
+
 const activeHaber = () => {
     carousel_indicators.getElementsByClassName("carousel_indicator")[0].classList.add("active");
     carousel_inner.getElementsByClassName("carousel-item")[0].classList.add("active");
 }
+
 const setHaberler = (imgHaber, numberHaber) => {
     carousel_indicators.innerHTML += `
     <li data-target="#carouselExampleIndicators" data-slide-to="${numberHaber}" class="carousel_indicator"></li>
@@ -46,8 +100,6 @@ const setHaberler = (imgHaber, numberHaber) => {
     carousel_inner.innerHTML += `<div class="carousel-item">
                                     <img class="d-block w-100"  src="${imgHaber}" alt="${numberHaber}. slide">
                                  </div>`;
-
-
 };
 
 const getHaberler = (callback) => {
@@ -70,6 +122,9 @@ const getHaberler = (callback) => {
 };
 
 // Call Functions
+getNumberFilm(getFilmler);
+
 getGununFilmi(setGununFilmi);
+
 getHaberler(activeHaber);
 
